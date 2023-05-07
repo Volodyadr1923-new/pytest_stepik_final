118e93a8a391c1785c6760acfd632b192564ff48
Volodyadr1923-new <vkadimirmakshin@gmail.com>
2023-05-07 20:18:57 +0300

    Handling an Exception on an Invalid Selector

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,18 @@
+from selenium.common.exceptions import NoSuchElementException
 # Создадим базовый класс
 class BasePage():
     # конструктор — метод, который вызывается, когда мы создаем объект
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
     # откроет нужную страницу в браузере, используя метод get()
     def open(self):
         self.browser.get(self.url)
+    # реализуем метод is_element_present, в котором будем перехватывать исключение
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -3,7 +3,10 @@ from selenium.webdriver.common.by import By
 
 # class MainPage(BasePage):
 class MainPage(BasePage):
-    # передаём не экземпляр браузера, а экземпляр класса
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+    # # передаём не экземпляр браузера, а экземпляр класса
+    # def go_to_login_page(self):
+    #     login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+    #     login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
